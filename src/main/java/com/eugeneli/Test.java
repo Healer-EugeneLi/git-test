e16f18a9d021d082925c6f0b4432f561fbb996f8
eugeneli <861269630@qq.com>
2021-10-16 16:07:08 +0800

    add test

---
+++ src/main/java/com/eugeneli/Test.java
@@ -0,0 +1,18 @@
+package com.eugeneli;
+
+/**
+ * @ClassName: Test
+ * @Description:
+ * @Author EugeneLi
+ * @Date: 2021/10/16
+ * @Time: 16:06
+ */
+public class Test {
+
+    public static void main(String[] args) {
+
+        System.out.println("111");
+        System.out.println("222");
+        System.out.println("333");
+    }
+}
