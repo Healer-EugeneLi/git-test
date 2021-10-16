842664ec18d6f2bd52331a2be62ca7278f40e8cc
eugeneli <861269630@qq.com>
2021-10-16 15:33:45 +0800

    people commit

---
+++ src/main/java/com/eugeneli/Pojo/People.java
@@ -0,0 +1,14 @@
+package com.eugeneli.Pojo;
+
+/**
+ * @ClassName: People
+ * @Description:
+ * @Author EugeneLi
+ * @Date: 2021/10/16
+ * @Time: 15:32
+ */
+public class People {
+
+    private String name;
+    private int age;
+}
